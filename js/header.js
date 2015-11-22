c5a94a528a219471e3773bc4698bcf3de840b7ea
Kristofer Rye <kristofer.rye@gmail.com>
2015-11-22 17:05:06 -0600

    Header: Add chevron and do a bit of design stuff.

--- js/header.js
+++ js/header.js
@@ -19,5 +19,34 @@ function Header(name, tagline, minHeight, classList) {
 		panel.appendChild(panel.text);
 	}
 
+	{
+		panel.chevron = document.createElement('div');
+		panel.chevron.classList.add('chevron');
+
+		{
+			var ns = 'http://www.w3.org/2000/svg';
+			var chevronSvg = document.createElementNS(ns, 'svg');
+			chevronSvg.setAttribute('width', '80');
+			chevronSvg.setAttribute('height', '20');
+
+			var g = document.createElementNS(ns, 'g');
+			g.setAttribute('transform', 'scale(20, 20)');
+
+			var path = document.createElementNS(ns, 'path');
+			path.setAttribute('d', 'M0.125 0.125L2.0 0.875L3.875 0.125');
+			path.setAttribute('fill', 'none');
+			path.setAttribute('stroke', '#aaaaaa');
+			path.setAttribute('stroke-width', '0.125');
+			path.setAttribute('stroke-linecap', 'round');
+			path.setAttribute('stroke-linejoin', 'round');
+
+			g.appendChild(path);
+			chevronSvg.appendChild(g);
+			panel.chevron.appendChild(chevronSvg);
+		}
+
+		panel.appendChild(panel.chevron);
+	}
+
 	return panel;
 };
