cb433331ef060e09f4924172be98ffb63b302885
Kristofer Rye <kristofer.rye@gmail.com>
2016-05-03 12:03:05 -0500

    Rip out the old redirection route and add new start syntax

--- config.ru
+++ config.ru
@@ -3,3 +3,5 @@ $LOAD_PATH.unshift(File.expand_path(File.join('lib'), File.dirname(__FILE__)))
 require 'site'
 
 server_configuration = Site::ServerConfiguration.new 'config/server.yml'
+
+run(Site::Server.start server_configuration)
--- lib/site/server.rb
+++ lib/site/server.rb
@@ -4,8 +4,12 @@ module Site
 
 	class Server < Sinatra::Base
 
-		get '/' do
-			redirect to 'index.html'
+		def self.start(server_configuration)
+			public_folder = File.expand_path(File.join(File.dirname(__FILE__), '..', '..', server_configuration.default_static_folder))
+
+			set :public_folder, public_folder
+
+			self
 		end
 
 	end
