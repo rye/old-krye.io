ec9d33e8799c7a3bfd4c9ac8be5a875ae73a324c
Kristofer Rye <kristofer.rye@gmail.com>
2016-05-07 14:16:38 -0500

    landing.js: Add a buncha comments

--- js/landing.js
+++ js/landing.js
@@ -1,4 +1,7 @@
+// Allocate a blank image for background loading.
 var internalImage = new Image();
+
+// Locate our background div.
 var landingBackgroundDiv = document.querySelector('.landing-background');
 
 // Prior to setting up callbacks and firing things off, we should first
@@ -6,8 +9,10 @@ var landingBackgroundDiv = document.querySelector('.landing-background');
 landingBackgroundDiv.style.opacity = 0.0;
 
 internalImage.onload = function() {
+	// Set the image background value before changing the opacity.
 	landingBackgroundDiv.style.backgroundImage = "url('" + internalImage.src + "')";
 	landingBackgroundDiv.style.opacity = 1.0;
 };
 
+// Specify our image and get things rolling.
 internalImage.src = 'Header Background.jpg';
