4a6d7717d5f1bf0917b39d57d4061a9b355cd835
Kristofer Rye <kristofer.rye@gmail.com>
2016-05-07 14:41:16 -0500

    ServerConfiguration: Remove helper methods

--- lib/site/server_configuration.rb
+++ lib/site/server_configuration.rb
@@ -8,64 +8,6 @@ module Site
 			super(filename)
 		end
 
-		def environment
-			sinatra['environment'].to_sym
-		end
-
-		def sessions?
-			sinatra['sessions']
-		end
-
-		def logging?
-			sinatra['logging']
-		end
-
-		def use_shitty_method_override?
-			sinatra['method_override']
-		end
-
-		def root_folder
-			File.join(File.dirname(@filename), sinatra['root'])
-		end
-
-		def serve_static?
-			sinatra['static']
-		end
-
-		def public_folder
-			dir = sinatra['public_folder']
-
-			Proc.new { File.join root, dir }
-		end
-
-		def views_folder
-			dir = sinatra['views_folder']
-
-			Proc.new { File.join root, dir }
-		end
-
-		def bind
-			sinatra['bind'] || 'localhost'
-		end
-
-		def port
-			sinatra['port'] || 4567
-		end
-
-		def show_exceptions
-			sinatra['show_exceptions'] || true
-		end
-
-		protected
-
-		def settings
-			self['settings'] || {}
-		end
-
-		def sinatra
-			settings['sinatra'] || {}
-		end
-
 	end
 
 end
