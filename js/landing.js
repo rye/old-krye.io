d8ee72516aee1afb4a4994db44b003e5c040bce0
Kristofer Rye <kristofer.rye@gmail.com>
2016-05-07 13:57:11 -0500

    index/landing: Add a bit more of the functionality code
    Now, with the nascent js/landing.js script, the page will load the image
    in the background (into a cached buffer variable which is unused, called
    "internalImage") before setting the "backgroundImage" property.
    
    The reason of doing this is that browsers can reuse images when they
    load them. So, if a browser loads the image in the background (as if it
    were creating a new Image object) and then just changes the image url on
    another object, that image will automagically get substituted in (after
    a couple of continuity checks by the browser to ensure that it hasn't
    changed)

---
+++ js/landing.js
@@ -0,0 +1,15 @@
+var internalImage = new Image();
+var landingBackgroundDiv = document.querySelector('.landing-background');
+
+// Prior to setting up callbacks and firing things off, we should first
+// make sure that the landing background is prepared.
+landingBackgroundDiv.style.opacity = 0.0;
+
+internalImage.onload = function() {
+	console.log('Image has loaded! Setting the landing background now.');
+
+	landingBackgroundDiv.style.backgroundImage = "url('" + internalImage.src + "')";
+	landingBackgroundDiv.style.opacity = 1.0;
+};
+
+internalImage.src = 'Header Background.jpg';
