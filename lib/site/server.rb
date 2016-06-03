00b4fd0dfc98c940ac9b1f38fdfc567c91ba62de
Kristofer Rye <kristofer.rye@gmail.com>
2016-06-02 22:23:28 -0500

    Move site over to testing version

--- lib/site/server.rb
+++ lib/site/server.rb
@@ -1,11 +1,16 @@
 require 'sinatra/base'
 
+require 'tilt/sass'
+require 'tilt/coffee'
+require 'tilt/erb'
+
 require 'site/logger'
-require 'site/compiler'
 
 module Site
 
 	class Server < Sinatra::Base
+
+		Tilt.register :"html.erb", Tilt[:erb]
 
 		# Default route to /
 		get '/' do
@@ -21,11 +26,45 @@ module Site
 			end
 		end
 
+		get '/:__route__.?:__extension__?' do
+			if File.readable?(filename = File.join(settings.views, params['__route__'] + (params['__extension__'] ? '.' + params['__extension__'] : '.html') + '.erb'))
+				@data = open(filename, 'rb') do |io|
+					io.read
+				end
+
+				erb @data
+			else
+				404
+			end
+		end
+
+		get '/js/:__script__.js' do
+			if File.readable?(filename = File.join(settings.views, 'js', params['__script__'] + '.coffee'))
+				@data = open(filename, 'rb') do |io|
+					io.read
+				end
+
+				coffee @data
+			else
+				404
+			end
+		end
+
+		get '/css/:__stylesheet__.css' do
+			if File.readable?(filename = File.join(settings.views, 'css', params['__stylesheet__'] + '.scss') || File.join(settings.views, 'css', params['__stylesheet__'] + '.sass'))
+				@data = open(filename, 'rb') do |io|
+					io.read
+				end
+
+				scss @data
+			else
+				404
+			end
+		end
+
 		# Runs all of the hooks necessary to prepare the server for execution.
 		def self.prepare!
 			setup!
-			compile!
-			run_compiler!
 		end
 
 		# Starts the server (this blocks until the server is killed; don't
@@ -40,42 +79,26 @@ module Site
 		def self.setup!
 			# Do a little bit of analysis to generate file paths.
 			@@root_folder = File.expand_path(File.join('..', '..'), File.dirname(__FILE__))
-			@@public_folder = File.expand_path(File.join(@@root_folder, 'tmp', '_static'))
-			@@skeleton_folder = File.expand_path(File.join(@@root_folder, 'static'))
+			@@public_folder = File.expand_path(File.join(@@root_folder, 'static'))
 			@@views_folder = File.expand_path(File.join(@@root_folder, 'views'))
-			@@__erb_folder__ = File.expand_path(File.join(@@views_folder, '__erb__'))
-			@@__scss_folder__ = File.expand_path(File.join(@@views_folder, '__scss__'))
-			@@__coffee_folder__ = File.expand_path(File.join(@@views_folder, '__coffee__'))
 
 			# Assert that all of the folders exist.
 			#
 			# * This should get cleaned up and made a bit more magical.
-			[@@root_folder, @@public_folder, @@skeleton_folder, @@views_folder, @@__erb_folder__, @@__scss_folder__, @@__coffee_folder__].each do |folder|
+			[@@root_folder, @@public_folder, @@views_folder].each do |folder|
 				raise RuntimeError, "Folder #{folder} does not exist!" unless File.directory? folder
 			end
 
 			# Set some settings so that Sinatra can find our static files.
 			set :root, @@root_folder
 			set :public_folder, @@public_folder
+			set :views, @@views_folder
 
 			# Don't show exceptions. (This could get ugly and bad in production)
 			set :show_exceptions, false
 
 			# Do log to the console.
 			set :logging, true
-
-			# Finally, create a new instance of `Compiler`, give it all of our folders.
-			@@compiler ||= Compiler.new(@@root_folder, @@public_folder, @@skeleton_folder, erb: @@__erb_folder__, scss: @@__scss_folder__, coffee: @@__coffee_folder__)
-		end
-
-		# Compiles the site.
-		def self.compile!
-			@@compiler.compile!
-		end
-
-		# Starts the Compiler.
-		def self.run_compiler!
-			@@compiler.run!
 		end
 
 		# Starts the Sinatra application.
