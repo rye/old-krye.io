78de83cfb453ee1e3e717a0dfc8ea22860d43352
Kristofer Rye <kristofer.rye@gmail.com>
2016-07-07 21:17:24 -0500

    Site::LOG_FILE: Synchronize with the fs

--- lib/site/logger.rb
+++ lib/site/logger.rb
@@ -5,6 +5,7 @@ require 'site/multi_delegator'
 module Site
 
 	LOG_FILE = File.open(File.expand_path(File.join('..', '..', '..', 'log', "site-#{File.basename $0}-#{Process.pid}.log"), __FILE__), 'a')
+	LOG_FILE.sync = true
 
 	# This is the logger that we use in our app.
 	Logger = Logger.new MultiDelegator.delegate(:write, :close).to(STDOUT, LOG_FILE)
