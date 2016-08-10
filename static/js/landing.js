ae1326e245e8800f78be522d2aeb28979a3c894e
Kristofer Rye <kristofer.rye@gmail.com>
2016-08-10 14:36:32 -0500

    js: Remove old JS and add Trianglificator

--- static/js/landing.js
+++ static/js/landing.js
@@ -1,18 +1,18 @@
-// Allocate a blank image for background loading.
-var internalImage = new Image();
-
-// Locate our background div.
-var landingBackgroundDiv = document.querySelector('.landing-background');
-
-// Prior to setting up callbacks and firing things off, we should first
-// make sure that the landing background is prepared.
-landingBackgroundDiv.style.opacity = 0.0;
-
-internalImage.onload = function() {
-	// Set the image background value before changing the opacity.
-	landingBackgroundDiv.style.backgroundImage = "url('" + internalImage.src + "')";
-	landingBackgroundDiv.style.opacity = 1.0;
-};
-
-// Specify our image and get things rolling.
-internalImage.src = 'Header Background.jpg';
+/* Allocate a blank image for background loading.
+ * var internalImage = new Image();
+ *
+ * // Locate our background div.
+ * var landingBackgroundDiv = document.querySelector('.landing-background');
+ *
+ * // Prior to setting up callbacks and firing things off, we should first
+ * // make sure that the landing background is prepared.
+ * landingBackgroundDiv.style.opacity = 0.0;
+ *
+ * internalImage.onload = function() {
+ * 	// Set the image background value before changing the opacity.
+ * 	landingBackgroundDiv.style.backgroundImage = "url('" + internalImage.src + "')";
+ * 	landingBackgroundDiv.style.opacity = 1.0;
+ * };
+ *
+ * // Specify our image and get things rolling.
+ * internalImage.src = 'Header Background.jpg';*/
