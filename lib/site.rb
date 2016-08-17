5c9283a25a1a8657dcdd37db60f010436ee0331b
Kristofer Rye <kristofer.rye@gmail.com>
2016-08-17 10:35:00 -0500

    Site: Add ROOT_DIRECTORY constant

--- lib/site.rb
+++ lib/site.rb
@@ -2,3 +2,9 @@ require 'site/cache'
 require 'site/logger'
 require 'site/multi_delegator'
 require 'site/server'
+
+module Site
+
+	ROOT_DIRECTORY = File.expand_path(File.join('..', '..'), __FILE__)
+
+end
