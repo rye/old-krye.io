ec255aa48e1b8351af1a785d660ec2fffe558111
Kristofer Rye <kristofer.rye@gmail.com>
2016-09-06 10:53:44 -0500

    logger_spec: Expand, add more examples

--- spec/lib/site/logger_spec.rb
+++ spec/lib/site/logger_spec.rb
@@ -12,9 +12,36 @@ require 'site/logger'
 
 describe Site do
 
-	it 'has a Logger constant which is a ::Logger' do
+	it 'has a LOG_FILE constant' do
+		expect(Site.const_defined?(:LOG_FILE)).to eq(true)
+	end
+
+	it 'has a Logger constant' do
 		expect(Site.const_defined?(:Logger)).to eq(true)
-		expect(Site::Logger).to be_a(::Logger)
+	end
+
+end
+
+describe 'Site::Logger' do
+
+	subject { Site::Logger }
+
+	it 'is a ::Logger' do
+		expect(subject).to be_a(::Logger)
+	end
+
+end
+
+describe 'Site::LOG_FILE' do
+
+	subject { Site::LOG_FILE }
+
+	it 'is a File' do
+		expect(subject).to be_a(File)
+	end
+
+	it 'is synchronized' do
+		expect(subject.sync).to be(true)
 	end
 
 end
