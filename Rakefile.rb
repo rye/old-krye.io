aa9e6f2f9c86046d72eb7bc8025f4e92797c5654
Kristofer Rye <kristofer.rye@gmail.com>
2017-01-25 10:10:39 -0600

    Add :tag task to Rakefile

--- Rakefile.rb
+++ Rakefile.rb
@@ -2,3 +2,9 @@ $LOAD_PATH.unshift(File.expand_path(File.join('..', 'lib'), __FILE__))
 
 require 'rspec/core/rake_task'
 RSpec::Core::RakeTask.new(:spec)
+
+task :tag do
+	IO.popen("ctags -eR --languages=ruby --verbose=yes --tag-relative=yes --exclude=.git --exclude=log .", "r") do |f|
+		$stdout.print f.read
+	end
+end
