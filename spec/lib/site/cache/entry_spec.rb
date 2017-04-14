e61dcbe80f282945927d0e710fd708803701f1bb
Kristofer Rye <kristofer.rye@gmail.com>
2017-04-14 15:32:37 -0500

    Add some things to the Entry spec

--- spec/lib/site/cache/entry_spec.rb
+++ spec/lib/site/cache/entry_spec.rb
@@ -9,3 +9,33 @@ describe 'lib/site/cache/entry.rb' do
 	end
 
 end
+
+require 'site/cache/entry'
+
+describe 'Site::Entry' do
+
+	subject do
+		Site::Entry
+	end
+
+	[:root_directory, :views_directory, :static_directory].each do |method|
+		describe ".#{method.to_s}" do
+			context 'taking no arguments' do
+
+				subject do
+					Site::Entry.send(method)
+				end
+
+				it 'returns a valid directory' do
+					expect(File.directory?(subject))
+				end
+
+			end
+		end
+	end
+
+	it 'inherits from Hash' do
+		expect(subject.ancestors).to include(Hash)
+	end
+
+end
