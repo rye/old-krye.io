ea56383481b3e324e0bd311b785777a2bae25e58
Kristofer Rye <kristofer.rye@gmail.com>
2017-04-14 15:53:55 -0500

    Add some rules to Rakefile and start collecting coverage data

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,8 +1,20 @@
 $LOAD_PATH.unshift(File.expand_path(File.join('..', 'lib'), __FILE__))
 
+desc "Runs spec and submits CodeClimate test coverage."
+task :ci do
+	[:spec, :codeclimate_test_report].each do |task|
+		Rake::Task[task].invoke
+	end
+end
+
+task :codeclimate_test_report do
+	system("bundle exec codeclimate-test-reporter")
+end
+
 require 'rspec/core/rake_task'
 RSpec::Core::RakeTask.new(:spec)
 
+desc "Generates ctags for the project."
 task :tag do
 
 	require 'site/logger'
