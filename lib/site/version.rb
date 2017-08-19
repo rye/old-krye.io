2063fc9be26a633f64147c104c7507353097efa5
Kristofer Rye <kristofer.rye@gmail.com>
2017-08-19 15:58:47 -0500

    Remove lines at beginning and end of modules
    This is in compliance with RuboCop, I guess.

--- lib/site/version.rb
+++ lib/site/version.rb
@@ -1,11 +1,7 @@
 module Site
-
 	class Version
-
 		def initialize(major:, minor: 0, patch: 0, prerelease: nil)
 			@major, @minor, @patch, @prerelease = major, minor, patch, prerelease
 		end
-
 	end
-
 end
